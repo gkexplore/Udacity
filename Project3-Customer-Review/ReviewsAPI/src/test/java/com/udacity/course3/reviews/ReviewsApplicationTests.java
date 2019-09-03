0bd773ad96eaf4b937ab030429f2d03aac9fda75
Karthik Ganesan <gkexplore@gmail.com>
2019-09-02 19:29:04 -0600

    using @DataJpaTest to avoid unnecessary config classes

--- Project3-Customer-Review/ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
+++ Project3-Customer-Review/ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
@@ -9,11 +9,9 @@ import com.udacity.course3.reviews.repository.ReviewRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,8 +20,7 @@ import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration(classes = { ReviewsJpaConfig.class }, loader = AnnotationConfigContextLoader.class)
+@DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReviewsApplicationTests {
 
